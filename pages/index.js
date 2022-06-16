5f66ee3c3e087252803bf72a5e58f14bcac56177
Yusuf Fauziyan <yusuffauziyan@gmail.com>
2022-06-17 01:01:26 +0700

    commit Dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Dashboard from "../components/Dashboard";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Dashboard />
       </main>
 
       <footer></footer>
