5fc22b62f6f92e0b716e710f36007fcbf7a7e8dc
tprayoga <tprayogaa@gmail.com>
2022-06-17 09:45:23 +0700

    commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,14 +3,26 @@ import { CgProfile } from "react-icons/cg";
 import { BsChevronExpand } from "react-icons/bs";
 import { BsSearch } from "react-icons/bs";
 import { BiCheck } from "react-icons/bi";
-import { Button, Menu } from "@mui/material";
+import { Button, Menu, MenuItem } from "@mui/material";
 import { CgAdd } from "react-icons/cg";
 
 const Navbar = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [dropdownOpen, setDropdownOpen] = React.useState(false);
+
+  const dropdown = Boolean(dropdownOpen);
   const open = Boolean(anchorEl);
+
+  const handleDropdown = (event) => {
+    setDropdownOpen(event.currentTarget);
+  };
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
+  };
+
+  const close = () => {
+    setDropdownOpen(null);
   };
   const handleClose = () => {
     setAnchorEl(null);
@@ -66,40 +78,25 @@ const Navbar = () => {
       <div className="flex justify-center items-center">
         <p className="mr-3 text-gray-600 border-2 p-2 cursor-pointer rounded-md hover:text-black hover:border-black">Feedback</p>
         <p className="mr-3 text-gray-600 cursor-pointer hover:text-black ">Docs</p>
-        <p id="basic-button1" aria-controls={open ? "basic-menu1" : undefined} aria-haspopup="true" aria-expanded={open ? "true" : undefined} onClick={handleClick} className=" cursor-pointer">
+        <p id="basic-button1" aria-controls={dropdownOpen ? "basic-menu1" : undefined} aria-haspopup="true" aria-expanded={dropdownOpen ? "true" : undefined} onClick={handleDropdown} className=" cursor-pointer">
           {" "}
           <CgProfile className=" text-3xl" />
         </p>
         <Menu
           id="basic-menu1"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
+          anchorEl={dropdownOpen}
+          open={dropdownOpen}
+          onClose={close}
           MenuListProps={{
             "aria-labelledby": "basic-button1",
           }}
         >
-          <div>
-            <div className="p-2 border-b-2">
-              <p>Dashboard</p>
-            </div>
-            <p className="px-5 py-2 text-gray-500 font-normal text-sm">Personal Account</p>
-            <div className=" cursor-pointer mx-2 py-2 flex justify-between rounded-md items-center hover:bg-gray-300">
-              <div className="flex items-center">
-                {" "}
-                <CgProfile className="pr-1 ml-1 text-3xl" />
-                <p className="font-bold"> User</p>
-              </div>
-              <div>
-                {" "}
-                <BiCheck className="text-2xl mx-2" />
-              </div>
-            </div>
-            <div className=" cursor-pointer mx-2 flex rounded-md items-center hover:bg-gray-300">
-              <CgAdd className="ml-1 pr-1 text-3xl my-2  text-sky-500" />
-              <p>Create Team</p>
-            </div>
-          </div>
+          <MenuItem>Dashboard</MenuItem>
+          <MenuItem>New Team</MenuItem>
+          <MenuItem>Settings</MenuItem>
+          <MenuItem>Theme</MenuItem>
+          <MenuItem>Command Menu</MenuItem>
+          <MenuItem>Logout</MenuItem>
         </Menu>
       </div>
     </div>
