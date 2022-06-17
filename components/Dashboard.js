a8dee50cb227e41c18c2991bcd1b5a9e9ccf90de
Yusuf Fauziyan <yusuffauziyan@gmail.com>
2022-06-17 10:29:05 +0700

    update

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -6,15 +6,15 @@ const Dashboard = () => {
 
   const handleClick = () => {}
   return (
-    <>
-       <div className="h-screen md:px-20 pl-2 pr-4 bg-[#FAFAFA]">
+    <div className='bg-[#FAFAFA]'>
+       <div className="container h-screen md:px-20 pl-2 pr-4 ">
         {/* SEARCH BAR */}
         <div className="flex mt-4 mb-4 w-full gap-4">
             <label class="relative w-full ">
                 <span class="absolute inset-y-0 left-0 flex items-center pl-2">
                 <BiSearchAlt2  className='text-slate-400 text-xl'/>
                 </span>
-                <input class="placeholder:text-p block border rounded py-1.5 pl-9 pr-3 focus:outline text-slate-200 w-full ease-in-out duration-100 " placeholder="Search..." type="text" name="search"/>
+                <input class="placeholder:text-p block border rounded py-1.5 pl-9 pr-3 focus:outline w-full ease-in-out duration-100 " placeholder="Search..." type="text" name="search"/>
             </label>
             <div className="bg-black flex items-center justify-center rounded hover:bg-white border border-black ease-in-out duration-150 group cursor-pointer sm:w-1/3 xl:w-1/5" >
                 <button className='text-white px-3 group-hover:text-black sm:hidden'>+</button>
@@ -53,7 +53,7 @@ const Dashboard = () => {
             </a>
         </div>
        </div>
-    </>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true
+    },
     extend: {},
   },
   plugins: [],
